7234c0bac44bbfad34ff8c7106147a4371b9569e
Rasheeda436 <rasheedamohammed1@gmail.com>
2025-06-12 16:25:29 +0530

    hello class updated

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,10 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("github session");
+        System.out.println("github session");
+        System.out.println("github session");
+        System.out.println("github session");
+        System.out.println("github session");
+
     }
 }
